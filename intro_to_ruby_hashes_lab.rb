a8b97e8a4f2243a351c9dd7fa2f252d81de17b42
Alec Reid <alecreid0093@gmail.com>
2019-09-16 05:19:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  base_hash = {
+   base_hash = {
    :railroads => {
      :pieces => 4,
      :rent_in_dollars => {
@@ -21,8 +21,10 @@ def monopoly_with_third_tier
        :two_pieces_owned => 50,
        :three_pieces_owned => 100,
        :four_pieces_owned => 200
-     }
-   }
+    },
+      :names=>
+    {}
+  }
  }
 end
 
